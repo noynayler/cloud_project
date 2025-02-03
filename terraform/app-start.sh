be128307ac14cc29404115d538cdcf426f90cbcd
Noy Nayler <noyn@checkpoint.com>
2025-02-03 03:04:28 +0200

    Added CI/CD workflow for Terraform and Docker deployment

--- terraform/app-start.sh
+++ terraform/app-start.sh
@@ -17,22 +17,22 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 
 # Pull the latest images from Docker Hub
 echo "Pulling backend and frontend images..."
-docker pull ${DOCKER_USERNAME}/backend:latest
-docker pull ${DOCKER_USERNAME}/frontend:latest
+docker pull ${DOCKER_USERNAME}/recipes-BE:latest
+docker pull ${DOCKER_USERNAME}/recipes-fe:latest
 
 # Stop & remove any existing containers
-docker ps -q --filter "name=backend" | grep -q . && docker stop backend || echo "No backend container to stop"
-docker ps -q --filter "name=frontend" | grep -q . && docker stop frontend || echo "No frontend container to stop"
+docker ps -q --filter "name=recipes-BE" | grep -q . && docker stop recipes-BE || echo "No backend container to stop"
+docker ps -q --filter "name=recipes-fe" | grep -q . && docker stop recipes-fe || echo "No frontend container to stop"
 
-docker ps -aq --filter "name=backend" | grep -q . && docker rm backend || echo "No backend container to remove"
-docker ps -aq --filter "name=frontend" | grep -q . && docker rm frontend || echo "No frontend container to remove"
+docker ps -aq --filter "name=recipes-BE" | grep -q . && docker rm recipes-BE || echo "No backend container to remove"
+docker ps -aq --filter "name=recipes-fe" | grep -q . && docker rm recipes-fe || echo "No frontend container to remove"
 
 # Run backend container on port 3001
 echo "Running backend container..."
-docker run -d --name backend -p 3001:3001 ${DOCKER_USERNAME}/backend:latest
+docker run -d --name recipes-BE -p 3001:3001 ${DOCKER_USERNAME}/recipes-BE:latest
 
 # Run frontend container on port 80 (serving on HTTP)
 echo "Running frontend container..."
-docker run -d --name frontend -p 80:3000 ${DOCKER_USERNAME}/frontend:latest
+docker run -d --name recipes-fe -p 80:3000 ${DOCKER_USERNAME}/recipes-fe:latest
 
 echo "Deployment Complete!"
