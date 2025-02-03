be9f41687a224a6bbafd68bc3d0feea515436e7f
Noy Nayler <noyn@checkpoint.com>
2025-02-03 15:45:00 +0200

    Deploying LB using terraform for backend

--- terraform/app-start.sh
+++ terraform/app-start.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
-set -e  # Exit on error
 
-# Update packages and install Docker
-echo "Updating packages and installing Docker..."
-sudo yum update -y
-sudo yum install -y docker
-sudo systemctl start docker
-sudo systemctl enable docker
+#export PATH=$PATH:/root/.nvm/versions/node/v20.13.0/bin
+#cd /opt/cloud_project/recipes-FE/ && . /root/envs.sh && nohup npm
+#cd /opt/cloud_project/recipes-BE/ && nohup serve -s build
 
-# Add `ec2-user` to Docker group (avoid sudo requirement)
-sudo usermod -aG docker ec2-user
 
-# Log in to Docker Hub
-echo "Logging into Docker Hub..."
-echo "${DOCKER_PWD}" | docker login -u "${DOCKER_USER}" --password-stdin
+#Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-# Pull the latest images from Docker Hub
-echo "Pulling backend and frontend images..."
-docker pull ${DOCKER_USER}/backend:latest
-#docker pull ${DOCKER_USER}/frontend:latest
+# Install serve globally
+npm install -g serve
 
-# Stop & remove any existing containers
-docker ps -q --filter "name=backend" | grep -q . && docker stop backend|| echo "No backend container to stop"
-#docker ps -q --filter "name=frontend" | grep -q . && docker stop frontend || echo "No frontend container to stop"
+# Clone backend and frontend repositories
+ git clone https://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@github.com/noynayler/cloud_project.git /opt/cloud_project
 
-docker ps -aq --filter "name=backend" | grep -q . && docker rm backend|| echo "No backend container to remove"
-#docker ps -aq --filter "name=frontend" | grep -q . && docker rm frontend || echo "No frontend container to remove"
 
-# Run backend container on port 3001
-echo "Running backend container..."
-docker run -d --name backend --network host -p 3001:3001 ${DOCKER_USER}/backend:latest
 
-# Run frontend container on port 80 (serving on HTTP)
-#echo "Running frontend container..."
-#docker run -d --name frontend --network host -p 80:3000 ${DOCKER_USER}/frontend:latest
+# Install backend dependencies and start server
+cd /opt/cloud_project/recipes-BE
+npm install
+npm start &
 
-echo "Deployment Complete!"
+# Install frontend dependencies, build, and start server
+cd /opt/cloud_project/recipes-FE
+npm install
+npm run build
+serve -s build &
